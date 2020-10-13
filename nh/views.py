592c7dfd48e7b790d07b736a17937ff1a2492ee0
ClaudeTJH <thojulcla@free.fr>
2020-10-13 14:14:59 +0200

    Update view interface

--- nh/views.py
+++ nh/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+# from django.template import RequestContext
+# from .models import LorenzMieForm
 
 def menu(request):
     """
@@ -28,4 +30,23 @@ def relance(request):
     """
     docstring
     """
+    if request.method == 'POST':
+        pass 
+        return render(request, 'pages/relance.html')
     return render(request, 'pages/relance.html')
+
+# def LorenzMieView(request):
+#     if request.method == 'POST':
+#         form = LorenzMieForm(request.POST)
+#         if form.is_valid():
+#             Submitted_Wavelength = form.cleaned_data[ 'Wavelength' ]
+#             Submitted_SphereRadius = form.cleaned_data[ 'SphereRadius' ]
+#             Submitted_RealPart = form.cleaned_data[ 'RealPart' ]
+#             Submitted_ImaginaryPart = form.cleaned_data[ 'ImaginaryPart' ]
+#             print (Submitted_Wavelength, Submitted_SphereRadius, \
+#             Submitted_RealPart, Submitted_ImaginaryPart)
+#             # Do something
+#     else:
+#         form = LorenzMieForm()
+#     return render(request, "pages/test.html", 
+#     {'form':form})
